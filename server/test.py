f7de33639726756e8c71787cea47f08d16bb047a
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-05-25 00:54:05 +0200

    update

--- server/test.py
+++ server/test.py
@@ -3,12 +3,12 @@ import yfinance as yf
 # returned columns ... Date, Open, High, Low, Close, Volume, Dividends, Stock Splits
 
 # Set the range and interval
-start_date = '2022-01-01'
-end_date = '2022-12-01'
-interval = '1d' # 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
+start_date = '2023-03-27'
+end_date = '2023-05-25'
+interval = '15m' # 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
 
 # Get the OHLC data
-eurusd = yf.Ticker('EURUSD=X')
+eurusd = yf.Ticker('GBPUSD=X')
 ohlc_data = eurusd.history(start=start_date, end=end_date, interval=interval)
 
 # Print the OHLC data
