d90cec596ad5cc3e88eadd4141568beeac299005
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-05-27 10:47:48 +0200

    added emails module

---
+++ server/emails.py
@@ -0,0 +1,8 @@
+# email confirmations on registration
+def send_registration_email_confirmation():
+
+# email confirmations on password recovery
+def send_password_recovery_email():
+
+# email confirmation on email change request
+def send_email_change_confirmation():
