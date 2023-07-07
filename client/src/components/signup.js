41e3a6ea96ef6b8cb26264cf1fd8060dc56cc490
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-07-07 20:28:55 +0200

    initialized code structure in js code files

---
+++ client/src/components/signup.js
@@ -0,0 +1,73 @@
+import React, { Component, useReducer } from 'react';
+import {
+    Collapse, 
+    Nav, NavItem, NavLink, 
+    UncontrolledDropdown, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, 
+    Input, InputGroup,
+    Button, Row, Col, Form, Container, Label
+} from "reactstrap";
+import { withCookies, Cookies } from 'react-cookie';
+import { instanceOf } from 'prop-types';
+import { Helmet } from 'react-helmet'
+import {
+    Audio,
+    BallTriangle,
+    Bars,
+    Circles,
+    Grid,
+    Hearts,
+    Oval,
+    Puff,
+    Rings,
+    SpinningCircles,
+    TailSpin,
+    ThreeDots,
+} from '@agney/react-loading';
+import { Platform_Name } from '../platform_name';
+import { Backend_Server_Address } from '../backend_server_url'
+
+class Signup extends Component{
+    static propTypes = {
+        cookies: instanceOf(Cookies).isRequired
+    };
+    constructor(props) { 
+        super(props);
+        this.state = {
+            loading: false
+        };
+
+        this.HandleChange = (e) => {
+            this.setState({[e.target.name]: e.target.value});
+        };
+    }
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <Helmet>
+                    <title>Signup | {Platform_Name}</title>
+                    {/* <meta name="description" content="" /> */}
+                </Helmet>
+                {
+                    this.state.loading == true
+                    ? <div>
+                        <br/><br/><br/>
+                        <h5 style={{color: '#1faced'}}>Loading...</h5>
+                        <br/><br/><br/>
+                        <TailSpin width='180px' style={{color: '#1faced'}}/>
+                    </div>
+                    : <div>
+                        
+                    </div>
+                }
+            </div>
+        );
+    }
+
+};
+
+export default withCookies(Signup);
---
+++ client/src/platform_name.js
@@ -0,0 +1,1 @@
+export const Platform_Name = 'Ocula Finance'
