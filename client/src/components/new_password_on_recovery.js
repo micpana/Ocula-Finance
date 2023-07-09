b6a3221536667ea57b257375a3eaaa91c92baaac
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-07-09 20:22:13 +0200

    updates, mainly in new_password_on_recovery.js

--- client/src/components/new_password_on_recovery.js
+++ client/src/components/new_password_on_recovery.js
@@ -36,7 +36,10 @@ class NewPasswordOnRecovery extends Component{
         super(props);
         this.state = {
             loading: false,
-            input_errors: {}
+            input_errors: {},
+            password: '',
+            password_confirmation: '',
+            screen: 'new password' // new password / ok / invalid token / expired / used / invalid password structure
         };
 
         this.HandleChange = (e) => {
@@ -86,6 +89,58 @@ class NewPasswordOnRecovery extends Component{
                 showIcon: true,
                 duration: 15000
             });
+        }
+
+        this.SetNewPassword = () => {
+            // initialize variable to store input validation status
+            var data_checks_out = true
+
+            // clear existing input errors if any
+            this.ClearInputErrors()
+
+            // validate input data
+            if (this.state.password === ''){ this.SetInputError('password', 'required'); data_checks_out = false }
+            if (this.IsPasswordStructureValid(this.state.password) === false){ this.SetInputError('password', 'invalid'); data_checks_out = false }
+            if (this.state.password_confirmation === ''){ this.SetInputError('password_confirmation', 'required'); data_checks_out = false }
+            if (this.state.password != this.state.password_confirmation){ this.SetInputError('password_mismatch', 'invalid'); data_checks_out = false }
+
+            // check data collection status
+            if (data_checks_out === false){ // user needs to check their input data
+                this.Notification('Check input fields for errors.', 'error')
+            }else{ // send data to server
+                this.setState({loading: true})
+
+                var data = new FormData()
+                data.append('token', this.props.match.params.recovery_token)
+                data.append('password', this.state.password)
+
+                axios.post(Backend_Server_Address + 'setNewPassword', data, { headers: { 'access_token': null }  })
+                .then((res) => {
+                    let result = res.data
+                    // set user email to state
+                    this.setState({screen: 'ok', loading: false})
+                }).catch((error) => {
+                    console.log(error)
+                    if (error.response){ // server responded with a non-2xx status code
+                        let status_code = error.response.status
+                        let result = error.response.data
+                        var notification_message = ''
+                        if(result === 'invalid token'){ this.setState({screen: 'invalid token'}) }
+                        else if (result === 'expired'){ this.setState({screen: 'expired'}) }
+                        else if (result === 'used'){ this.setState({screen: 'used'}) }
+                        else if (result === 'invalid password structure'){ this.setState({screen: 'invalid password structure'}) }
+                        else{
+                            notification_message = 'Apologies! The server encountered an error while processing your request (Error ' + status_code.toString() + ': ' + result + '). Please try again later or contact our team for further assistance.'
+                            this.Notification(notification_message, 'error')
+                        }
+                    }else if (error.request){ // request was made but no response was received ... network error
+                        this.Notification('Oops! It seems there was a problem with the network while processing your request. Please check your internet connection and try again.', 'error')
+                    }else{ // error occured during request setup ... no network access
+                        this.Notification("We're sorry but it appears that you don't have an active internet connection. Please connect to the internet and try again.", 'error')
+                    }
+                    this.setState({loading: false})
+                })
+            }
         }
     }
 
