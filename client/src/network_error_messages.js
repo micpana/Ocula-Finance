41905898837debc6928ae499faa9169ad4400601
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-08-05 21:59:25 +0200

    updated network_error_messages.js

--- client/src/network_error_messages.js
+++ client/src/network_error_messages.js
@@ -1,7 +1,7 @@
 import { Platform_Name } from './platform_name';
 
 // server responded with a unknown non-2xx status code ... to be appended by actual server error message received
-const unknown_non_2xx_message = 'Apologies! The server encountered an error while processing your request. Please try again later or contact our team for further assistance.'
+const unknown_non_2xx_message = 'Apologies! The server encountered an error while processing your request. Please try again later or contact our team for further assistance if the problem persists.'
 export const Unknown_Non_2xx_Message = unknown_non_2xx_message
 
 // request was made but no response was received ... network error
