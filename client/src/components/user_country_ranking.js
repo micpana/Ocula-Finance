fc24d97c11982943d99092ff1ffc6d1f4458dd40
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-08-05 21:10:34 +0200

    added network_error_messages.js, network error messages to be imported from there

--- client/src/components/user_country_ranking.js
+++ client/src/components/user_country_ranking.js
@@ -26,6 +26,7 @@ import {
 import { Platform_Name } from '../platform_name';
 import { Backend_Server_Address } from '../backend_server_url';
 import { Access_Token_Cookie_Name } from '../access_token_cookie_name';
+import { Unknown_Non_2xx_Message, Network_Error_Message, No_Network_Access_Message } from '../network_error_messages';
 import { Message, useToaster } from "rsuite";
 
 class UserCountryRanking extends Component{
---
+++ client/src/network_error_messages.js
@@ -0,0 +1,13 @@
+import { Platform_Name } from './platform_name';
+
+// server responded with a unknown non-2xx status code ... to be appended by actual server error message received
+const unknown_non_2xx_message = 'Apologies! The server encountered an error while processing your request. Please try again later or contact our team for further assistance.'
+export const Unknown_Non_2xx_Message = unknown_non_2xx_message
+
+// request was made but no response was received ... network error
+const network_error_message = 'Oops! It seems there was a problem with the network while processing your request. Please check your internet connection and try again.'
+export const Network_Error_Message = network_error_message
+
+// error occured during request setup ... no network access
+const no_network_access_message = "We're sorry but it appears that you don't have an active internet connection. Please connect to the internet and try again."
+export const No_Network_Access_Message = no_network_access_message
