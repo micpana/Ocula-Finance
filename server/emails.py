b10883bf574a75bb7e4fa34d2ac2d8410e1ac4d1
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-09-28 22:11:27 +0200

    updates, maily in emails.py

--- server/emails.py
+++ server/emails.py
@@ -8,15 +8,55 @@ def send_crafted_email(user_email, email_html, email_text):
 
 # email confirmations on registration
 def send_registration_email_confirmation(user_email, username, verification_token, token_expiration_date):
+    # email html
+    email_html = 
+
+    # email text
+    email_text = 
+
+    # send crafted email
+    send_crafted_email(user_email, email_html, email_text)
 
 # email confirmations on password recovery
 def send_password_recovery_email(user_email, username, recovery_token, token_expiration_date):
+    # email html
+    email_html = 
+
+    # email text
+    email_text = 
+
+    # send crafted email
+    send_crafted_email(user_email, email_html, email_text)
 
 # email confirmation on email change request
 def send_email_change_confirmation(user_email, username, verification_token, token_expiration_date):
+    # email html
+    email_html = 
+
+    # email text
+    email_text = 
+
+    # send crafted email
+    send_crafted_email(user_email, email_html, email_text)
 
 # login on new device email notification
 def send_login_on_new_device_email_notification(user_email, username, user_os, user_device, user_ip_address, user_browser):
+    # email html
+    email_html = 
+
+    # email text
+    email_text = 
+
+    # send crafted email
+    send_crafted_email(user_email, email_html, email_text)
 
 # account email change email notification
 def send_account_email_change_email_notification(user_email, username, user_os, user_device, user_ip_address, user_browser):
+    # email html
+    email_html = 
+
+    # email text
+    email_text = 
+
+    # send crafted email
+    send_crafted_email(user_email, email_html, email_text)
