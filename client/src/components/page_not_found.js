a81eb64a515f83798fad33df80bff821a1ee95ac
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-10-03 15:39:51 +0200

    added axios imports mostly in .js files using axios

--- client/src/components/page_not_found.js
+++ client/src/components/page_not_found.js
@@ -27,6 +27,7 @@ import {
 import { Platform_Name } from '../platform_name';
 import { Backend_Server_Address } from '../backend_server_url';
 import { Access_Token_Cookie_Name } from '../access_token_cookie_name';
+import axios from 'axios';
 import { Unknown_Non_2xx_Message, Network_Error_Message, No_Network_Access_Message } from '../network_error_messages';
 import LoadingScreen from './loading_screen';
 import InputErrors from './input_errors';
