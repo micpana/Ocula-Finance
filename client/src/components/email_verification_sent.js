d92b64ecf90367ae851851ea570adc1b5ed2d1f2
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-10-19 13:25:38 +0200

    removed notification function that had been left in email_verification_sent.js

--- client/src/components/email_verification_sent.js
+++ client/src/components/email_verification_sent.js
@@ -205,19 +205,6 @@ class EmailVerificationSent extends Component{
         }
     }
 
-    Notification = (message, message_type) => { // message type -> info / success / warning / error
-        const toaster = useToaster();
-        
-        // push notification message
-        toaster.push(<Message>{message}</Message>, {
-            placement: 'topCenter',
-            closable: true,
-            type: message_type,
-            showIcon: true,
-            duration: 15000
-        });
-    }
-
     componentDidMount() {
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
             this.setState({
