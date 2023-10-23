ebd0bdd7d31e64fcd09832ce886527fb823e0c7d
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-10-23 18:37:24 +0200

    updates to user_count_chart.js

---
+++ client/src/components/user_count_chart.js
@@ -0,0 +1,241 @@
+import React, { Component, useReducer } from 'react';
+import {
+    Collapse, 
+    Nav, NavItem, NavLink, 
+    UncontrolledDropdown, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, 
+    Input, InputGroup, InputGroupText,
+    Button, Row, Col, Form, Container, Label
+} from "reactstrap";
+import { withCookies, Cookies } from 'react-cookie';
+import { instanceOf } from 'prop-types';
+import { Helmet } from 'react-helmet'
+import {
+    Audio,
+    BallTriangle,
+    Bars,
+    Circles,
+    Grid,
+    Hearts,
+    Oval,
+    Puff,
+    Rings,
+    SpinningCircles,
+    TailSpin,
+    ThreeDots,
+} from '@agney/react-loading';
+import { ToastContainer, toast } from 'react-toastify';
+import { Platform_Name } from '../platform_name';
+import { Backend_Server_Address } from '../backend_server_url';
+import { Access_Token_Cookie_Name } from '../access_token_cookie_name';
+import axios from 'axios';
+import { Unknown_Non_2xx_Message, Network_Error_Message, No_Network_Access_Message } from '../network_error_messages';
+import LoadingScreen from './loading_screen';
+import InputErrors from './input_errors';
+import Notification from './notification_alert';
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart, Bar, Cell} from 'recharts';
+import { FaCalendarDay, FaCalendarWeek } from 'react-icons/fa';
+
+class UserCountChart extends Component{
+    static propTypes = {
+        cookies: instanceOf(Cookies).isRequired
+    };
+    constructor(props) { 
+        super(props);
+        this.state = {
+            loading: false,
+            input_errors: {},
+            on_mobile: false,
+            start_date: '',
+            end_date: '',
+            category: 'Daily', // Daily / Monthly / Yearly
+            categories: ['Daily', 'Monthly', 'Yearly'],
+            user_count_statistics: [
+                {
+                    date: '14/11/2023',
+                    users: 30
+                },
+                {
+                    date: '15/11/2023',
+                    users: 300
+                },
+                {
+                    date: '16/11/2023',
+                    users: 1200
+                }
+            ]
+        };
+
+        this.HandleChange = (e) => {
+            this.setState({[e.target.name]: e.target.value});
+        };
+
+        this.SetInputError = (field, error) => { // error -> required / invalid
+            // existing errors
+            var existing_errors = this.state.input_errors
+
+            // existing errors modified
+            existing_errors[field] = error
+
+            // update state
+            this.setState({input_errors: existing_errors})
+        }
+
+        this.ClearInputErrors = () => {
+            // existing errors
+            var existing_errors = this.state.input_errors
+            // array of existing error field names
+            var existing_error_fields = Object.keys(existing_errors)
+            // set existing error fields to undefined, clearing them
+            existing_error_fields.map((item, index) => {
+                existing_errors[item] = undefined
+            })
+            this.setState({input_errors: existing_errors})
+        }
+
+        this.GetUserCountStatistics = () => {
+            const { cookies } = this.props;
+            this.setState({loading: true})
+
+            var data = new FormData()
+            data.append('start_date', this.state.start_date)
+            data.append('end_date', this.state.end_date)
+            data.append('category', this.state.category)
+
+            axios.post(Backend_Server_Address + 'getUserCountStatistics', data, { headers: { 'access_token': cookies.get(Access_Token_Cookie_Name) }  })
+            .then((res) => {
+                let result = res.data
+                // set users to state
+                this.setState({user_count_statistics: result, loading: false})
+            }).catch((error) => {
+                console.log(error)
+                if (error.response){ // server responded with a non-2xx status code
+                    let status_code = error.response.status
+                    let result = error.response.data
+                    var notification_message = ''
+                    if(
+                        result === 'Access token disabled via signout' ||
+                        result === 'Access token expired' ||
+                        result === 'Not authorized to access this' ||
+                        result === 'Invalid token'
+                    ){ 
+                        // delete token from user cookies
+                        cookies.remove(Access_Token_Cookie_Name, { path: '/' });
+                        // redirect to sign in
+                        let port = (window.location.port ? ':' + window.location.port : '');
+                        window.location.href = '//' + window.location.hostname + port + '/signin';
+                    }else{
+                        notification_message = Unknown_Non_2xx_Message + ' (Error '+status_code.toString()+': '+result+')'
+                        Notification(notification_message, 'error')
+                    }
+                }else if (error.request){ // request was made but no response was received ... network error
+                    Notification(Network_Error_Message, 'error')
+                }else{ // error occured during request setup ... no network access
+                    Notification(No_Network_Access_Message, 'error')
+                }
+                this.setState({loading: false})
+            })
+        }
+    }
+
+    componentDidMount() {
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            this.setState({
+                on_mobile: true
+            })
+        }
+        // this.GetUserCountStatistics()
+    }
+
+    render() {
+        return (
+            <div>
+                <Helmet>
+                    <title>User Count Chart | {Platform_Name}</title>
+                    {/* <meta name="description" content="" /> */}
+                </Helmet>
+                <ToastContainer />
+                {
+                    this.state.loading === true
+                    ? <LoadingScreen />
+                    : <div>
+                        <br/>
+                        <h5 style={{fontWeight: 'bold'}}>
+                            User Count Chart
+                        </h5>
+                        <br/><br/>
+                        <Row style={{margin: '0px'}}>
+                            <Col sm='3' style={{textAlign: 'left', marginRight: '20px'}}>
+                                <Label style={{fontWeight: 'bold'}}>Category:</Label>
+                                <select name='category' value={this.state.category} onChange={this.HandleChange}
+                                    style={{border: 'none', borderBottom: '1px solid #F2B027', width: '100%', backgroundColor: 'inherit', color: '#00539C', outline: 'none'}}
+                                >
+                                    {
+                                        this.state.categories.map((item) => {
+                                            return<option value={item}>{item}</option>
+                                        })
+                                    }
+                                </select>
+                            </Col>
+                        </Row>
+                        <br/>
+                        <Row style={{margin: '0px'}}>
+                            <Col sm='3' style={{textAlign: 'left', marginRight: '20px'}}>
+                                <Label style={{color: '#00539C'}}>Start Date</Label>
+                                <InputGroup>
+                                    <InputGroupText addonType="prepend">
+                                        <FaCalendarDay style={{margin:'10px'}}/>
+                                    </InputGroupText>
+                                    <Input style={{border: 'none', color: 'inherit', backgroundColor: 'inherit'}}
+                                        name="start_date" id="start_date"
+                                        value={this.state.start_date} onChange={this.HandleChange} type="date" 
+                                    />
+                                </InputGroup>
+                            </Col>
+                            <Col sm='3' style={{textAlign: 'left', marginRight: '30px'}}>
+                                <Label style={{color: '#00539C'}}>End Date</Label>
+                                <InputGroup>
+                                    <InputGroupText addonType="prepend">
+                                        <FaCalendarWeek style={{margin:'10px'}}/>
+                                    </InputGroupText>
+                                    <Input style={{border: 'none', color: 'inherit', backgroundColor: 'inherit'}}
+                                        name="end_date" id="end_date"
+                                        value={this.state.end_date} onChange={this.HandleChange} type="date"  
+                                    />
+                                </InputGroup>
+                            </Col>
+                            <Col sm='3'>
+                                <br/>
+                                <Button onClick={this.GetUserCountStatistics} 
+                                    style={{border: '1px solid #00539C', borderRadius: '20px', color: '#ffffff', fontWeight: 'bold', backgroundColor: '#00539C'}}
+                                >
+                                    View
+                                </Button>
+                            </Col>
+                        </Row>
+                        <br/><br/><br/>
+                        <div style={{width: '100%', overflowX: 'scroll'}}>
+                            <LineChart
+                                width={1000}
+                                height={300}
+                                data={this.state.user_count_statistics}
+                                margin={{
+                                    top: 0, right: 0, left: 0, bottom: 0,
+                                }}
+                            >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="date" />
+                                <YAxis />
+                                <Tooltip />
+                                <Legend />
+                                <Line type="monotone" dataKey="users" stroke="#00539C" activeDot={{ r: 8 }}/>
+                            </LineChart>
+                        </div>
+                    </div>
+                }
+            </div>
+        );
+    }
+
+};
+
+export default withCookies(UserCountChart);
