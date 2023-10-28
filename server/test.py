8ef3f21c25a3d7626a3870b1c62de474f1590f0b
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-10-28 22:25:35 +0200

    added network_error_screens.js

--- server/test.py
+++ server/test.py
@@ -1,4 +1,4 @@
-def getText(username):
+def getTex(username):
     text = """
 yes {username}
 now now
@@ -8,6 +8,8 @@ nice
     print(username)
     return text
 
+getText = getTex
+print(getText)
 txt = getText('james')
 print(txt)
 txt = txt.replace('\n', '<br/>')
