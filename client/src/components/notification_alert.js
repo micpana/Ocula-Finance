57120df9a4010314e5b3dbb00fee40834afca5c2
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-11-12 21:26:32 +0200

    frontend, notification alerts now closing after a user closes them, and not automatically

--- client/src/components/notification_alert.js
+++ client/src/components/notification_alert.js
@@ -5,7 +5,7 @@ export default function useNotification (message, message_type){ // message type
     toast(message, {
         position: "top-center",
         type: message_type,
-        autoClose: 5000,
+        autoClose: false, // for auto close insert number of milliseconds eg 5000 for auto close in 5 seconds
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
