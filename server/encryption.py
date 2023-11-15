55f4c7ee8bd31c22bb0b6504d8a0999b1203ec24
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-11-15 22:34:55 +0200

    backend, removed other specifications in requirements.txt

--- server/encryption.py
+++ server/encryption.py
@@ -1,9 +1,9 @@
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(
-        schemes=["pbkdf2_sha256"],
-        default="pbkdf2_sha256",
-        pbkdf2_sha256__default_rounds=30000
+    schemes=["pbkdf2_sha256"],
+    default="pbkdf2_sha256",
+    pbkdf2_sha256__default_rounds=30000
 )
 
 def encrypt_password(password):
