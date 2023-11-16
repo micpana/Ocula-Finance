3a0e65505575f1857ecb8d58c6761f86dac77a82
Michael Panashe Mudimbu <michaelmudimbu@gmail.com>
2023-11-17 00:37:48 +0200

    backend, updates to yahoo_finance_data.py

--- server/yahoo_finance_data.py
+++ server/yahoo_finance_data.py
@@ -29,20 +29,28 @@ def yahoo_fetch_data(symbol, timeframes, data_collection_days):
         # set interval ... valid = 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
         if timeframe == 'Monthly':
             interval = '1mo'
+            date_column_name = ''
         elif timeframe == 'Weekly':
             interval = '1wk'
+            date_column_name = ''
         elif timeframe == 'Daily':
             interval = '1d'
+            date_column_name = 'Date'
         elif timeframe == 'H4':
             interval = '1h' # yfinance has no 4hour timeframe
+            date_column_name = 'Datetime'
         elif timeframe == 'H1':
             interval = '1h'
+            date_column_name = 'Datetime'
         elif timeframe == 'M15':
             interval = '15m'
+            date_column_name = 'Datetime'
         elif timeframe == 'M5':
             interval = '5m'
+            date_column_name = 'Datetime'
         elif timeframe == 'M1':
             interval = '1m'
+            date_column_name = 'Datetime'
         else:
             print('Timeframe not configured:', timeframe)
             quit()
@@ -50,13 +58,18 @@ def yahoo_fetch_data(symbol, timeframes, data_collection_days):
         # Get the OHLC data
         symbol_ticker = yf.Ticker(symbol+'=X')
         timeframe_ohlc_df = symbol_ticker.history(start=timezone_from, end=timezone_to, interval=interval)
+        
+        # reset index into a column and rename columns to match those in mt5_data.py
+        timeframe_ohlc_df.reset_index(inplace=True)
+        timeframe_ohlc_df = timeframe_ohlc_df.rename(
+            columns={date_column_name: 'time', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close'}
+        )
 
-        # if timeframe = H4, modify timeframe_ohlc_df into a 4h timeframe, we collected 1h data since yfinance doesn't have 4h
+        if timeframe = H4, modify timeframe_ohlc_df into a 4h timeframe, we collected 1h data since yfinance doesn't have 4h
         if timeframe == 'H4':
             # set the datetime index if its not already set
             timeframe_ohlc_df['time'] = pd.to_datetime(timeframe_ohlc_df['time'])
             timeframe_ohlc_df.set_index('time', inplace=True)
-            print(timeframe_ohlc_df.head())
 
             # turn into H4 df
             h4_df = timeframe_ohlc_df.resample('4H').agg({'open': 'first', 'high': 'max', 'low': 'min', 'close': 'last'})
@@ -83,3 +96,8 @@ def yahoo_fetch_data(symbol, timeframes, data_collection_days):
 
     # return data as timeframe 1, timeframe 2, timeframe 3, timeframe 4
     return timeframe_1, timeframe_2, timeframe_3, timeframe_4
+
+# get data
+data1, data2, data3, data4 = yahoo_fetch_data('EURUSD', ['Daily', 'H4', 'H1', 'M15'], 50) # symbol, timeframes, data collection days
+
+print('\nDaily:\n', data1, '\nH4:\n', data2, '\nH1:\n', data3, '\nM15:\n', data4)
