830304b48bd826496e2a1e9894806fe7fb5799a7
micpana <michaelmudimbu@gmail.com>
2024-10-08 14:38:47 +0200

    updates

--- server/test.py
+++ server/test.py
@@ -1,11 +1,5 @@
-animals = [{'hunts': 'yes', 'name': 'cat'}, 
-           {'hunts': 'yes', 'name': 'cat'}, 
-           {'hunts': 'maybe', 'name': 'rabbit'}, 
-           {'hunts': 'no', 'name': 'horse'},
-           {'hunts': 'no', 'name': 'cow'}
-           ]
+import operator
 
-# Find the index where 'hunts' is 'no'
-index = next((i for i, animal in enumerate(animals) if animal['hunts'] == 'no' and animal['name'] == 'cow'), None)
+use = operator.gt
 
-print(index)
+print(use(2, 1))
