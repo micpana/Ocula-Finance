205953f5b01bd1675fb4b3383396265f07d1f4b4
micpana <michaelmudimbu@gmail.com>
2024-11-21 20:06:06 +0200

    update

--- client/src/credentials.js
+++ client/src/credentials.js
@@ -3,7 +3,7 @@
 // EmailJs ***********************************************************************************************************************
 export const EmailJsServiceID = () => {
     // get service id
-    const service_id = process.env.EmailJsServiceID
+    const service_id = process.env.REACT_APP_EmailJsServiceID
 
     // return service id
     return service_id
@@ -11,7 +11,7 @@ export const EmailJsServiceID = () => {
 
 export const EmailJsTemplateID = () => {
     // get template id
-    const template_id = process.env.EmailJsTemplateID
+    const template_id = process.env.REACT_APP_EmailJsTemplateID
 
     // return template id
     return template_id
@@ -19,7 +19,7 @@ export const EmailJsTemplateID = () => {
 
 export const EmailJsAPIKey = () => {
     // get api key
-    const api_key = process.env.EmailJsAPIKey
+    const api_key = process.env.REACT_APP_EmailJsAPIKey
 
     // return api key
     return api_key
